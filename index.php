11c60ea58df727a8580816a87f5787c4b1c5039d
Huxley Dias <huxley.dias@hotmail.com>
2016-07-08 11:17:49 -0300

    go makers

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>HandMade UX</title>
+        <meta name="description" content=".">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width">
+        <meta name="Author" content="Huxley Dias">
+        <meta name="Distribution" content="Global">
+        <meta name="Language" content="pt-br">
+        <meta name="rating" content="General">
+        <link rel="canonical" href="http://www.handmadeux.com.br"/>
+
+        <!-- for facebook -->
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="HandMade UX"/>
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="http://www.handmadeux.com.br/img/avatar-handmade.jpg" />
+        <meta property="og:url" content="http://www.hundmadeux.com.br"/>
+        <!-- for twitter -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:site" content="">
+        <meta name="twitter:title" content="HandMade UX"> 
+        <meta name="twitter:description" content=""> 
+        <meta name="twitter:creator" content=""> 
+        <meta name="twitter:image:src" content=""> 
+        <meta name="twitter:domain" content="">
+        <link rel="icon" type="image/png" href="img/favicon.png" />
+        <link rel="canonical" href=""/> 
+        <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/main.css">
+        <!-- Google Tag Manager -->
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WX3C4Q"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WX3C4Q');</script>
+        <!-- End Google Tag Manager -->
+    </head>
+    <body itemscope itemtype="http://schema.org/Organization">
+
+                <div class="main">
+                    <!-- header; -->
+                    <section class="container" id="header">
+                        <div class="row-fluid">
+                            <h1 class="brand">HandMade UX
+                                <img src="img/hand-made-ux-marca.png" alt="AndMade UX" width="300" height="74">
+                            </h1>
+
+                            <nav class="top-nav"></nav>
+                        </div>
+
+                        <div class="feature">
+                            <h2 class="feature-text"><small>Design Your Life</small></h2>
+                            <h2 class="feature-text">Criamos Experiências Incríveis Juntos</h2>
+                            <p class="feature-text feature-text-paragraph">Com resultados 100% mensuráveis e qualidade garantida pela validação com usuários reais.</p>
+                            <div class="box-cta">
+                                <a href="#" class="feature-cta" title="Verja Mais">VER MAIS</a>
+                            </div>
+                        </div>
+
+                        
+                    </section>
+                    <!-- :header -->
+
+                    <!-- make-experience: -->
+                    <section class="container" id="make-experience">
+                        <div class="row-fluid">
+                            <p class="text-default">PROJETOS INOVADORES E ESTRTÉGICOS</p>
+                            <h2 class="title-section">Experiências que criamos </br>com nossos clientes</h2>
+
+                        </div>
+                        <div class="row-fluid">
+                            <div class="col col-md-6">
+                                
+                            <div class="col col-md-6">
+                                
+                            </div>
+                        </div>
+                    </section>
+                    <!-- :make-experience -->
+
+                    <!-- case-1: -->
+                    <section class="container" id="case-1">
+                        <div class="row-fluid">
+                            
+                                
+                            </div>
+                        </div>
+                    </section>
+                    <!-- :case-1 -->
+
+                    <!-- case-2: -->
+                    <section class="container" id="case-2">
+                        <div class="row-fluid">
+                            <div class="col col-md-6">
+                                
+                            <div class="col col-md-6">
+                                
+                            </div>
+                        </div>
+                    </section>
+                    <!-- :case-2 -->
+
+                    <!-- clientes: -->
+                    <section class="container" id="clientes">
+                        <div class="row-fluid">
+                            <div class="col col-md-6">
+                                
+                            <div class="col col-md-6">
+                                
+                            </div>
+                        </div>
+                    </section>
+                    <!-- :clientes -->
+
+                    <!-- semana-1: -->
+                    <section class="container" id="case-2">
+                        <div class="row-fluid">
+                            <div class="col col-md-6">
+                                
+                            <div class="col col-md-6">
+                                
+                            </div>
+                        </div>
+                    </section>
+                    <!-- :semana-1 -->
+
+                    <!-- semana-2: -->
+                    <section class="container" id="semana-2">
+                        <div class="row-fluid">
+                            <div class="col col-md-6">
+                                
+                            <div class="col col-md-6">
+                                
+                            </div>
+                        </div>
+                    </section>
+                    <!-- :semana-2 -->
+
+                    <!-- semana-3: -->
+                    <section class="container" id="semana-3">
+                        <div class="row-fluid">
+                            <div class="col col-md-6">
+                                
+                            <div class="col col-md-6">
+                                
+                            </div>
+                        </div>
+                    </section>
+                    <!-- :semana-3 -->
+
+                    <!-- case-2: -->
+                    <section class="container" id="design-conections">
+                        <div class="row-fluid">
+                            <div class="col col-md-6">
+                                
+                            <div class="col col-md-6">
+                                
+                            </div>
+                        </div>
+                    </section>
+                    <!-- :case-2 -->
+
+                    <!-- case-2: -->
+                    <section class="container" id="footer">
+                        <div class="row-fluid">
+                            <div class="col col-md-6">
+                                
+                            <div class="col col-md-6">
+                                
+                            </div>
+                        </div>
+                    </section>
+                    <!-- :case-2 -->
+
+
+
+                   
+                    
+                </div>
+                <!--script src="//code.jquery.com/jquery-1.11.0.min.js"></script-->
+                <script src="js/jquery-1.10.2.min.js"></script>
+                <script src="js/modernizr.custom.js"></script>
+                <script src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
+                <script src="js/jquery.backgroundvideo.min.js"></script>
+                <script src="js/script.js"></script>
+    </body>
+</html>
