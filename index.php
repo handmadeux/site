6acdbf98a710ce6b4831f650734e4ed120037f8c
Huxley Dias <huxley.dias@hotmail.com>
2016-07-12 16:18:34 -0300

    refinamento de fontes e imagens finais

--- index.php
+++ index.php
@@ -29,17 +29,17 @@
         <meta name="twitter:domain" content="">
         <link rel="icon" type="image/png" href="img/favicon.png" />
         <link rel="canonical" href=""/> 
-        <!-- <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'> -->
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Montserrat:200,400,700' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,200' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.0/animate.min.css">
         <link rel="stylesheet" href="css/main.css">
         <!-- Google Tag Manager -->
-        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WX3C4Q"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- 
             <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WX3C4Q');</script>
+        })(window,document,'script','dataLayer','GTM-WX3C4Q');</script> -->
         <!-- End Google Tag Manager -->
     </head>
     <body itemscope itemtype="http://schema.org/Organization">
@@ -49,15 +49,15 @@
                     <section class="container" id="header">
                         <div class="row-fluid">
                             <h1 class="brand">HandMade UX
-                                <img src="img/hand-made-ux-marca.png" alt="AndMade UX" width="300" height="74">
+                                <img src="img/hand-made-ux-marca.png" alt="HandMade UX" width="200" height="32">
                             </h1>
 
                             <nav class="top-nav"></nav>
                         </div>
 
                         <div class="feature">
-                            <h2 class="feature-text text-white"><small>Design Your Life</small></h2>
-                            <h2 class="feature-text text-white">Criamos Experiências Incríveis Juntos</h2>
+                            
+                            <h2 class="feature-text text-white">Criamos experiências</br> incríveis Juntos</h2>
                             <p class="feature-text feature-text-paragraph text-white">Com resultados 100% mensuráveis e qualidade garantida pela validação com usuários reais.</p>
                             <div class="box-cta">
                                 <a href="#" class="feature-cta" title="Verja Mais">VER MAIS</a>
@@ -72,8 +72,8 @@
                     <section class="container" id="make-experience">
                         
                         <div class="row-fluid head-line">
-                            <p class="text-default">PROJETOS INOVADORES E ESTRTÉGICOS</p>
-                            <h2 class="title-section">Experiências que criamos </br>com nossos clientes</h2>
+                            <p class="text-default">DESTAQUES</p>
+                            <h2 class="title-section">Projetos inovadores</br> e estratégicos</h2>
                         </div>
 
                         <div class="row-fluid">
@@ -104,13 +104,13 @@
                                     <p class="default-text case-client-name">OPTIMA</p>
                                     <h3 class="case-project-name">Digital Transformation Strategy & Mobile App</h3>
                                     <p class="default-text case-text-description">Com resultados 100% mensuráveis e qualidade garantida pela validação com usuários reais. Conheça o que estamos criando.</p>
-                                    <a href="#" class="cta" title="Ver Projeto"> VER PROJETO</a>
+                                    <a href="#" class="cta cta-play-video" title="Play vídeo do evento">Vídeo do evento <span><i class="arrow-right"></i></span></a>
                                 </div>
                             </div>
 
                             <div class="col col-md-6">
                                 <div class="frame-video-case-leve-me">
-                                    
+                                    <span class="bt-play-video"><i class="arrow-right"></i></span>
                                 </div>
                             </div>
                            
@@ -127,7 +127,7 @@
                                 <h3 class="case-project-name">Mobile App & Wearable</h3>
                                 <p class="default-text case-text-description">Com resultados 100% mensuráveis e qualidade garantida pela validação com usuários reais.</p>
                                 <div class="box-cta">
-                                    <a href="#" class="cta cta-dark" title="Ver Projeto">VER PROJETO</a>
+                                    <a href="#" class="cta" title="Ver Projeto">VER PROJETO</a>
                                 </div>
                             </div>
                         </div>
@@ -168,40 +168,93 @@
                     </section>
                     <!-- :clientes -->
 
-                    <!-- semana-1: -->
-                    <section class="container" id="semana-1">
+                     <!-- metodo: -->
+                    <section class="container" id="metodo">
+                        
+                        <div class="row-fluid head-line">
+                            <p class="text-default">MÉTODO</p>
+                            <h2 class="title-section">Estratégia e design mobile</br> em 15 dias</h2>
+                        </div>
+
+                           <!-- <div class="box-cta">
+                                <a id="demo01" href="#animatedModal" class="feature-cta" title="Verja Mais">VER MODAL</a>
+                            </div>
+
+                            <div class="time-line">
+                                <ol class="sprint-time-line">
+                                    <li class="list-card">
+                                        <header>
+                                            <h3>Header Card</h3>
+                                            <div class="conector-cart">
+                                                <span>
+                                                    <i></i>
+                                                </span>
+                                            </div>
+                                        </header>
+                                        <content>
+                                            <img src="http://placehold.it/320x180" alt="Titulo Imagem">
+                                            <p class="description-card">Com resultados 100% mensuráveis e qualidade garantida pela validação com usuários reais.</p>
+                                        </content>
+                                    </li>
+                                </ol>
+                            </div> -->
+                            
                         <div class="row-fluid">
-                                
+                            <div class="col col-md-6">
+                                <div class="frame-video-metodo pull-right"></div>
+                            </div>
+                            <div class="col col-md-6">
+                                <ul class="metodo-list">
+                                    <li>Prazo previsível</li>
+                                    <li>Custo previsível</li>
+                                    <li>Estrutura necessária previsível</li>
+                                    <li>Etapas de aprovação eliminadas</li>
+                                    <li>Quantidade garantida pela validação com usuários reais e equipe 100% sênior com dedicação exclusiva</li>
+                                    <li>Resultados 100% mensuráveis</li>
+                                </ul>
                             </div>
                         </div>
-                    </section>
-                    <!-- :semana-1 -->
 
-                    <!-- semana-2: -->
-                    <section class="container" id="semana-2">
-                        <div class="row-fluid">
-                            <div class="col col-md-6">
-                                
-                            <div class="col col-md-6">
-                                
-                            </div>
+
+                    </section>
+                    <!-- :metodo -->
+
+                    <!-- modal: -->
+                    <div id="animatedModal">
+
+                        <a href="" class="close-animatedModal" title="fechar detalhes">X</a>
+
+
+                        <div class="modal-content">
+                            <!-- semana-1: -->
+                            <section class="container" id="semana-1">
+                                <div class="row-fluid">
+
+                                </div>
+                            </section>
+                            <!-- :semana-1 -->
+
+                            <!-- semana-2: -->
+                            <section class="container" id="semana-2">
+                                <div class="row-fluid">
+
+                                </div>
+                            </section>
+                            <!-- :semana-2 -->
+
+                            <!-- semana-3: -->
+                            <section class="container" id="semana-3">
+                                <div class="row-fluid">
+
+                                </div>
+                            </section>
+                            <!-- :semana-3 -->
                         </div>
-                    </section>
-                    <!-- :semana-2 -->
 
-                    <!-- semana-3: -->
-                    <section class="container" id="semana-3">
-                        <div class="row-fluid">
-                            <div class="col col-md-6">
-                                
-                            <div class="col col-md-6">
-                                
-                            </div>
-                        </div>
-                    </section>
-                    <!-- :semana-3 -->
+                    </div>
+                    <!-- :modal -->
 
-                    <!-- case-2: -->
+                    <!-- connections: -->
                     <section class="container" id="design-conections">
                         <div class="row-fluid">
 
@@ -221,30 +274,135 @@
                             
                         </div>
                     </section>
-                    <!-- :case-2 -->
+                    <!-- :connections -->
 
-                    <!-- case-2: -->
-                    <section class="container" id="footer">
-                        <div class="row-fluid">
-                            <div class="col col-md-6">
+
+                    <!-- idealizadores: -->
+                    <section class="container" id="idealizadores">
+                       
+                        <div class="row">
+
+
+                                <p class="default-text head-line">O NOVO CAMINHO DO DESIGN</p></br>
+                                <h2 class="title-section head-line">Idealizadores desse</br> grande projeto</h2>
+                            
+                            <div class="idealizador">
+                                <div class="col col-md-6 idealizador-img">
+                                    <img class="pull-right" src="img/luis-felipe-fernandes.jpg" alt="Foto de Luis Felipe Fernandes">
+                                </div>
                                 
-                            <div class="col col-md-6">
-                                
+                                <div class="col col-md-6 idealizador-info">
+                                    <h3 class="idealizador-name">Luis Felipe Fernandes</h3>
+                                    <p class="idealizador-bio text-default">Luis Felipe é UX Designer há vários anos, desenhando produtos e campanhas digitais para grandes marcas. Formado em Publicidade pela PUC-Campinas e com MBA em Gestão pela FGV, foi premiado como Melhor Profissional de UX do Mercado Digital em 2014 e 2015, com passagens pelas agências Casa, JWT e AlmapBBDO. Atuou na área de inovação do Banco Itaú e é também Professor de UX na Fiap e UX Academy.</p>
+                                </div>
                             </div>
+
+                            <div class="idealizador">
+                                
+                                <div class="col col-md-6 idealizador-info">
+                                    <h3 class="idealizador-name">Gustavo Burnier</h3>
+                                    <p class="idealizador-bio text-default">Gustavo trabalhou e viveu por 22 anos em New York, Londres, Madrid, Buenos Aires e Paris.  Com MBA do INSEAD, foi Co-Fundador e CMO do Decolar.com, Head de Marketing do eBay na Espanha, e em Londres foi Global Business Director da JWT e Global Digital Director na WPP.  Desde que voltou ao Brasil em 2011, foi Sócio, Diretor de Gestão e Operações Digitais da AlmapBBDO. O foco da sua carreira é a transformação digital dos negócios.</p>
+                                </div>
+
+                                <div class="col col-md-6 idealizador-img">
+                                    <img class="pull-left" src="img/gustavo-burnier.jpg" alt="Foto de Gustavo Burnier">
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </section>
+                    <!-- :idealizadores -->
+
+                    <!-- footer: -->
+                    <section class="container" id="contato">
+                        <div class="row-fluid">
+                            
+                            <p class="default-text head-line">CONTATO</p></br>
+                            <h2 class="title-section head-line">Entre em contato</br> a gente</h2>
+
+                            <form name="contact-fomr" method="post" class="contact-form">
+                                <label for="name">
+                                    <input class="input-full" type="text" name="name" value="" placeholder="Nome">
+                                </label>
+                                <label for="email">
+                                    <input class="input-full" type="mail" name="email" value="" placeholder="E-mail">
+                                </label>
+                                <label for="msg">
+                                    <textarea class="input-full" name="name" value="" placeholder="Mensagem"></textarea> 
+                                </label>
+                                <label class="box-cta">
+                                    <input type="submit" name="enviar" value="ENVIAR" class="cta">
+                                </label>
+
+                            </form>
+
                         </div>
                     </section>
-                    <!-- :case-2 -->
+                    <!-- :footer -->
+                    
 
+                    <!-- footer: -->
+                    <section class="container" id="footer">
+                        <div class="row">
+                            <img src="http://placehold.it/160x70" alt="HandMade UX" class="footer-brand">
+                            <nav class="footer-nav">
+                                <ul class="footer-nav-list">
+                                    <li class="nav-item">
+                                        <a href="#" title="">Projetos</a>
+                                    </li>
+                                     <li class="nav-item">
+                                        <a href="#" title="">O Que Fazemos</a>
+                                    </li>
+                                     <li class="nav-item">
+                                        <a href="#" title="">Método</a>
+                                    </li>
+                                     <li class="nav-item">
+                                        <a href="#" title="">Quem Somos</a>
+                                    </li>
+                                     <li class="nav-item">
+                                        <a href="#" title="">Contato</a>
+                                    </li>
+                                </ul>
+                            </nav>
 
+                            <p class="default-text copyright">Copyright © 2016 HandMade UX - Todos os Direitos Reservados.</p>
 
-                   
+                            <nav class="social-nav">
+                                <ul class="social-list">
+                                    <li class="social-item">
+                                        <a href="#" title="Linkedin HandMade UX"></a>
+                                    </li>
+                                    <li class="social-item">
+                                    <a href="#" title="Facebook HandMade UX"></a>
+                                    </li>
+                                </ul>
+                            </nav>
+
+                        </div>
+                    </section>
+                    <!-- :footer -->
                     
                 </div>
-                <!--script src="//code.jquery.com/jquery-1.11.0.min.js"></script-->
-                <script src="js/jquery-1.10.2.min.js"></script>
+                
+                <script src="js/jquery.min.js"></script>
+                <script src="js/animatedModal.js"></script>
                 <script src="js/modernizr.custom.js"></script>
                 <script src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
                 <script src="js/jquery.backgroundvideo.min.js"></script>
                 <script src="js/script.js"></script>
+                <script>
+
+            
+            //demo 02
+            $("#demo01").animatedModal({
+                modalTarget:'animatedModal',
+                animatedIn:'lightSpeedIn',
+                animatedOut:'bounceOutDown',
+                color:'#eeeeee'
+            });
+
+        </script>
     </body>
 </html>
