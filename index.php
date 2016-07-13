2269ce9bd2573cc816d39c6f072d315ef0c1514a
Huxley Dias <huxley.dias@hotmail.com>
2016-07-13 18:01:51 -0300

    adicionando logos clientes e novas imagens cases

--- index.php
+++ index.php
@@ -41,6 +41,14 @@
             '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-WX3C4Q');</script> -->
         <!-- End Google Tag Manager -->
+
+        <!-- start Mixpanel -->
+        <script type="text/javascript">
+            (function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config reset people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
+                for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=e.createElement("script");a.type="text/javascript";a.async=!0;a.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";f=e.getElementsByTagName("script")[0];f.parentNode.insertBefore(a,f)}})(document,window.mixpanel||[]);
+                    mixpanel.init("8b065a43fa93cbec992eb6e8e88f2fea");
+        </script>
+        <!-- end Mixpanel -->
     </head>
     <body itemscope itemtype="http://schema.org/Organization">
 
@@ -509,6 +517,8 @@
                 $('#play-video-optima-1, #play-video-optima-2').on('click', function () {
                     $('#play-video-optima-2').hide();
                     player.playVideo();
+                    //Mixpanel test
+                    mixpanel.track("Video play - Case Optima");
 
                 });
 
@@ -521,6 +531,16 @@
                     });
                 });
 
+                // Mixpanel test
+
+                //Definindo um Profile
+                mixpanel.identify("000001");
+                mixpanel.people.set({
+                    "$first_name": "Huxley",
+                    "$last_name": "Dias",
+                    "$created": "2016-07-13T15:00:00",
+                    "$email": "huxley@handmadeux.com.br"
+                });
 
                 </script>
 
