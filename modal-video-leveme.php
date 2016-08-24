cc85c78a22c6d3d68d9f1ace1c6d7b57d297ee25
Huxley Dias <huxley.dias@hotmail.com>
2016-08-24 17:56:29 -0300

    otimizando tamanho das imagens

---
+++ modal-video-leveme.php
@@ -0,0 +1,18 @@
+<!-- modal: -->
+<div id="animatedModal">
+
+    <a href="" class="close-animatedModal" title="fechar detalhes">X</a>
+
+    <!-- modal content: -->
+    <div class="modal-content">
+        <!-- videoframe: -->
+        <section class="container" id="semana-1">
+            <div class="row-fluid">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/ux0p3OAnVdE" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </section>
+        <!-- :vieoframe -->
+    </div>
+    <!-- :modal content -->
+</div>
+<!-- :modal -->
